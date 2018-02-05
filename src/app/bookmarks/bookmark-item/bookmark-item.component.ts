1c140020f99effd6ae1e2fef5d7cdc1d93be1c44
Łukasz Wilk <wilklukaszz@gmail.com>
2018-02-05 21:48:04 +0100

    bookmarks imip

--- src/app/bookmarks/bookmark-item/bookmark-item.component.ts
+++ src/app/bookmarks/bookmark-item/bookmark-item.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, ViewChild } from '@angular/core';
+import { Bookmark } from '../bookmark.model';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-bookmark-item',
@@ -6,10 +8,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./bookmark-item.component.css']
 })
 export class BookmarkItemComponent implements OnInit {
+  @Input() bookmark: Bookmark;
+  @ViewChild('f') editForm: NgForm;
 
   constructor() { }
 
   ngOnInit() {
+    setTimeout(() => {
+      this.editForm.setValue({
+        name: this.bookmark.name,
+        url: this.bookmark.url,
+      })
+    }, 500)
+
   }
 
 }
